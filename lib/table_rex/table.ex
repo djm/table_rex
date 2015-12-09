d4f82304eb4c5439c64c5dd6e3417ffdc1409723
Darian Moody <mail@djm.org.uk>
2015-12-09 21:02:43 +0000

    Allow the default Column meta used to be user-set
    Before, if a valid Column was not found at a given index then
    the hardcoded default was provided. Now, the Table struct has
    an extra key which stores the user-set default.
    
    Functions to change this user-set default are still to come.

--- lib/table_rex/table.ex
+++ lib/table_rex/table.ex
@@ -2,7 +2,7 @@ defmodule TableRex.Table do
   alias TableRex.Column
   alias TableRex.Table
 
-  defstruct title: nil, header_row: [], rows: [], columns: %{}
+  defstruct title: nil, header_row: [], rows: [], columns: %{}, default_column: %Column{}
 
   @type t :: %__MODULE__{}
 
@@ -10,14 +10,16 @@ defmodule TableRex.Table do
   Retreives the column struct containing column meta at a given col_index.
   If one does not exist, a default %TableRex.Column{} is returned.
   """
+  @spec get_column(Table.t, integer) :: Column.t
   def get_column(%Table{} = table, col_index) when is_number(col_index) do
-    Dict.get(table.columns, col_index, %Column{})
+    Dict.get(table.columns, col_index, table.default_column)
   end
 
   @doc"""
   Retreives the value of a column meta option at the specified col_index.
   If no value has been set, default values are returned.
   """
+  @spec get_column_meta(Table.t, integer, atom) :: any
   def get_column_meta(%Table{} = table, col_index, key) when is_number(col_index) and is_atom(key) do
     get_column(table, col_index)
     |> Map.fetch!(key)
@@ -26,6 +28,7 @@ defmodule TableRex.Table do
   @doc"""
   Returns a boolean detailing if the passed table has any (expected) row data set.
   """
+  @spec has_rows?(Table.t) :: boolean
   def has_rows?(%Table{rows: []}), do: false
   def has_rows?(%Table{rows: rows}) when is_list(rows), do: true
 
