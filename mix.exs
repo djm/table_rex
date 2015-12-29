50eede9039f973a2665b92bbd91518b068a06dea
Darian Moody <mail@djm.org.uk>
2015-12-29 01:42:50 +0000

    Update hex related metadata

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule TableRex.Mixfile do
       app: :table_rex,
       name: "table_rex",
       source_url: "https://github.com/djm/table_rex",
+      description: "Generate configurable text-based tables for display (ASCII & more)",
       version: "0.1.0",
       elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
@@ -25,5 +26,13 @@ defmodule TableRex.Mixfile do
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev}]
+  end
+
+  defp package do
+    [maintainers: ["Darian Moody"],
+     licenses: ["MIT"],
+     links: [
+       {"GitHub", "https://github.com/djm/table_rex"}
+     ]]
   end
 end
