70d75d842b64d9b111d2f2022bdb74781ec2453c
Darian Moody <mail@djm.org.uk>
2020-04-18 17:42:01 +0100

    Fix tests

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,13 @@
 use Mix.Config
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+# This configuration is loaded before any dependency and is restricted
+# to this project. If another project depends on this project, this
+# file won't be loaded nor affect the parent project, which is what
+# we want.
+
+# IO.ANSI checks for shell compatability before enabling ANSI escape
+# codes & this is a problem for our CI running on Github Actions as it
+# doesn't support ANSI yet. Therefore we configure it to force it
+# being enabled (as we don't care about seeing the output, just that
+# the tests check it works).
+config :elixir, ansi_enabled: true
--- config/test.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-# IO.ANSI checks for shell compatability before enabling
-# ANSI escape codes & this is a problem for our CI running
-# on Github Actions as it doesn't support ANSI yet. Therefore
-# we configure it to force it being enabled (as we don't care
-# about seeing the output, just that the tests check it works).
-config :elixir, ansi_enabled: true
