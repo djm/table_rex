104728977e175b2e015242e13d64b91fe15ed886
Darian Moody <mail@djm.org.uk>
2020-04-18 17:30:15 +0100

    Enable ANSI

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,5 @@
 use Mix.Config
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
