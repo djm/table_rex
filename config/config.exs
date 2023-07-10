e5b4438dd9fee0a96f05a66c16b46a6a438a55c9
Darian Moody <mail@djm.org.uk>
2023-07-10 22:15:55 +0100

    Remove usage of deprecated Mix.Config.config/2

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,11 @@
-use Mix.Config
+import Config
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project, which is what
 # we want.
 
-# IO.ANSI checks for shell compatability before enabling ANSI escape
+# IO.ANSI checks for shell compatibility before enabling ANSI escape
 # codes & this is a problem for our CI running on Github Actions as it
 # doesn't support ANSI yet. Therefore we configure it to force it
 # being enabled (as we don't care about seeing the output, just that
